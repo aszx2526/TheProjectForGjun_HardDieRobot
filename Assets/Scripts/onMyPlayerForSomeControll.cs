39bafa0d1d653a7226c138efd297769c82f6278c
aszx2526 <tsai2526@hotmail.com>
2016-10-29 15:14:45 +0800

    自寫腳色控制
    因為用內建那個第三人稱腳色控制
    跳起來落地之後都會頓一下
    沒有辦法很流暢的跳跑跳跑
    所以就自己寫了一個新版的腳色控制
    不過有問題
    使用虛擬鍵盤來進行操控的時候
    手感很奇怪
    感覺還是要用內建的才好使

---
+++ Assets/Scripts/onMyPlayerForSomeControll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class onMyPlayerForSomeControll : MonoBehaviour {
+    public float myMoveSpeed = 10.0F;
+    public float myRotationSpeed = 100.0F;
+    public float myJumpHeigh;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float rotation = Input.GetAxis("Horizontal") * myRotationSpeed;
+        //float translation = Input.GetAxis("Vertical") * myMoveSpeed;
+        float h = CrossPlatformInputManager.GetAxis("Horizontal")* myRotationSpeed;
+        float v = CrossPlatformInputManager.GetAxis("Vertical")* myMoveSpeed;
+        h *= Time.deltaTime;
+        v *= Time.deltaTime;
+        transform.Rotate(0, h, 0);
+        transform.Translate(0, 0, v);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            myJumpFN();
+        }
+    }
+    public void myJumpFN() {
+        if (GetComponent<Rigidbody>().velocity.y == 0) {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, myJumpHeigh, 0));
+        }
+    }
+}
